9be7fa561080297a6057d0cad8a3d68ab3f4ed00
Olivia Liu <olivialiu@dhcp-vl2041-30682.eduroam.cornell.edu>
2024-04-23 15:08:47 -0400

    added Q3

---
+++ Q3_RScript.R
@@ -0,0 +1,50 @@
+#load necessary package
+library(data.table)
+library(dplyr)
+
+# Set the root directory
+root_directory <- "/Users/olivialiu/Desktop/4520-Final-Project/raw_data"
+
+# List all CSV files within the root directory
+files <- list.files(root_directory, pattern = "\\.txt$", full.names = TRUE, recursive = TRUE)
+
+# use function to rbind all dataset into a dataset
+all_data <- rbindlist(lapply(files, fread))
+
+
+all_data <- all_data %>%
+  select(1:11)
+
+#rename columns 
+all_data <- all_data %>%
+  rename(WBANNO = V1) %>%
+  rename(LST_DATE = V2) %>%
+  rename(CRX_VN = V3 ) %>%
+  rename(LONGITUDE = V4) %>%
+  rename(LATITUDE = V5) %>%
+  rename(T_DAILY_MAX = V6) %>%
+  rename(T_DAILY_MIN = V7) %>%
+  rename(T_DAILY_MEAN = V8) %>%
+  rename(T_DAILY_AVG = V9) %>%
+  rename(P_DAILY_CALC = V10) %>%
+  rename(SOLARAD_DAILY = V11)
+
+#load the csv file containing each station's summary information
+summary <- read.csv("/Users/olivialiu/Desktop/4520-Final-Project/stations_info.csv")
+
+#match two dataset based on the station identifier number
+matched_indices <- match(all_data$WBANNO, summary$Identifier)
+
+#create state variable & station_name variable and fill in information based on the matching
+all_data$state <- summary$State[matched_indices]
+all_data$station_name <- summary$StationName[matched_indices]
+
+
+#change all missing values into NAs (turn extreme values into NAs)
+all_data[all_data < -1000] <- NA
+
+#rearrange the order of columns
+all_data <- all_data %>%
+  select(WBANNO, state, station_name,everything())
+
+write.csv(all_data, "/Users/olivialiu/Desktop/all_daily_data.csv")
